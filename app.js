05234b97ff6b545e9fdd984489919d610d6b1972
rohitkolapkar <rohitkolapkar333@gmail.com>
2019-03-14 18:41:23 +0530

    sign in with google added

--- app.js
+++ app.js
@@ -4,10 +4,24 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var passport=require('passport');
+var session=require('express-session');
+var GoogleStategy=require('passport-google-oauth').OAuth2Strategy;
+var auth=require('./routes/auth');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+passport.use(new GoogleStategy({
+  clientID:'475173781688-59sbiklu3nsmj1q8skaitovsld9jt84t.apps.googleusercontent.com',
+  clientSecret:'ba0f4HhC7yDMEfes6_oBrX44',
+  callbackURL:'http://localhost:3000/auth/google/callback'},
+  function(req,accessToken,refreshToken,profile,done){
+    done(null,profile);
+  }
+));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +33,25 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//express-session
+app.use(session({secret:'anything'}));
+
+//passport initialization
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user,done){
+  done(null,user);
+
+});
+
+passport.deserializeUser(function(user,done){
+  done(null,user);
+})
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth',auth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
